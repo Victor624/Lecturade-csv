117b30e3ffbcaba5d871ee831935c7e4f13f4097
Victor624 <victorvargas023@gmail.com>
2022-10-31 01:18:49 -0300

    covid

--- datacsv.py
+++ datacsv.py
@@ -1,3 +1,9 @@
-ef run():
-  data = read_csv.read_csv('./app/data.csv')
-  data = list(filter(lambda item : item['Continent'] == 'South America',data))
+import csv
+
+def run(data):
+
+  with open(data,"r") as datos:
+    reader=csv.reader(datos, delimiter=",")
+    cabecera=next(reader)
+
+    print(cabecera)
